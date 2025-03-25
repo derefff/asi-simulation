a0889578ba6a90078e6affc3de8413569ead2d98
derefff <deref122@gmail.com>
2025-03-25 20:39:06 +0100

    simple simulation of magnetization

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
 
 // Boltzman constant
 // what's that? cpp shenanigans I see
@@ -15,7 +16,8 @@ struct spin
   int value;
   bool isVertical;
   // near neighbour like list
-  spin* neighbourList;
+  int neighbourListIndex_J1[2];
+  int neighbourListIndex_J2[4];
 };
 
 struct lattice
@@ -27,21 +29,21 @@ struct lattice
 
 spin* generate_spins(int L)
 {
-  // one dimmensional table
+  // one-dimensional table
   spin* spins = (spin*)malloc(sizeof(spin) * (L * L));
 
-  int spin_id = 0;
+  // int spin_id = 0;
 
   for (int i = 0; i < L * L; i++)
   {
     // define spin id
-    spins[i].id = spin_id;
+    // spins[i].id = spin_id;
     // vertical-> i%2 = 1
     spins[i].isVertical = (i % 2 == 0) ? false : true;
     // assign spin random orientation (+1 or -1)
     spins[i].value = -1 + 2 * (rand() % 2);
 
-    spin_id++;
+    // spin_id++;
   }
 
   // generate near neighbour list for each spin
@@ -50,7 +52,34 @@ spin* generate_spins(int L)
     int row = j / L;      // y
     int column = (j % L); // x
 
-    // std::cout << row << "," << column << "\n"; //debug info
+    if (spins[j].isVertical)
+    {
+      // vertical case
+      // look for J1 interactions (above and below, periodic)
+      spins[j].neighbourListIndex_J1[0] = (row == 0) ? (L * (L - 1) + column) : (j - L);
+      spins[j].neighbourListIndex_J1[1] = (row == L - 1) ? column : (j + L);
+
+      // look for J2 interactions (left and right, periodic)
+      spins[j].neighbourListIndex_J2[0] = (column == 0) ? (j + (L - 1)) : (j - 1);
+      spins[j].neighbourListIndex_J2[1] = (column == L - 1) ? (j - (L - 1)) : (j + 1);
+      spins[j].neighbourListIndex_J2[2] = spins[j].neighbourListIndex_J2[0] + L;
+      spins[j].neighbourListIndex_J2[3] = spins[j].neighbourListIndex_J2[1] + L;
+    }
+    else
+    {
+      // horizontal case
+      // look for J1 interactions (left and right, periodic)
+      spins[j].neighbourListIndex_J1[0] = (column <= 1) ? (row * L) + (L - (2 - column)) : (j - 2);
+      spins[j].neighbourListIndex_J1[1] = (column >= L - 2) ? (row * L) + (column % 2) : (j + 2);
+
+      // look for J2 interactions (above and below, periodic)
+      spins[j].neighbourListIndex_J2[0] = (row == 0) ? (L * (L - 1) + column + 1) : (j - L + 1);
+      spins[j].neighbourListIndex_J2[1] = (row == 0) ? (L * (L - 1) + column - 1) : (j - L - 1);
+      spins[j].neighbourListIndex_J2[2] = (row == L - 1) ? (column + 1) : (j + L + 1);
+      spins[j].neighbourListIndex_J2[3] = (row == L - 1) ? (column - 1) : (j + L - 1);
+    }
+
+    // std::cout << row << "," << column << "\n"; // debug info
   }
 
   return spins;
@@ -68,14 +97,61 @@ lattice* create_lattice(int L)
   return lat;
 }
 
+double compute_energy_difference(lattice* lat, int spin_index, double J, double T)
+{
+  spin s = lat->spins[spin_index];
+  int sum_neighbors = 0;
+  for (int i = 0; i < 2; i++)
+    sum_neighbors += lat->spins[s.neighbourListIndex_J1[i]].value;
+  for (int i = 0; i < 4; i++)
+    sum_neighbors += lat->spins[s.neighbourListIndex_J2[i]].value;
+
+  return 2 * J * s.value * sum_neighbors;
+}
+
+void glauber_step(lattice* lat, double J, double T)
+{
+  int random_spin = rand() % (lat->width * lat->height-1);
+
+  double dE = compute_energy_difference(lat, random_spin, J, T);
+  if (dE < 0 || exp(-dE / (Kb * T)) > ((double)rand() / RAND_MAX))
+    lat->spins[random_spin].value *= -1;
+}
+
+double compute_magnetization(lattice* lat)
+{
+  int total_spin = 0;
+  int num_spins = lat->width * lat->height;
+  for (int i = 0; i < num_spins; i++)
+    total_spin += lat->spins[i].value;
+  return (double)total_spin / num_spins;
+}
+
 int main(int argc, char* argv[])
 {
-  const int L = 4;
-  // Create simulation board
+  const int L = 70;
+  const double J = 1.0;
+  const double T = 1.8;
   lattice* board = create_lattice(L);
-
+  
+  std::ofstream outfile("magnetization_data.txt");
+  if (!outfile) {
+    std::cerr << "Error opening file!" << std::endl;
+    return 1;
+  }
+  
+  for (int step = 0; step < 10'000; step++) {
+    glauber_step(board, J, T);
+    double magnetization = compute_magnetization(board);
+    outfile << step << " " << magnetization << std::endl;
+  }
+  
+  outfile.close();
+  std::cout << "Magnetization data saved to magnetization_data.txt" << std::endl;
+  
   return 0;
 }
+
 // 1. Create simulation board
 // a) Define board size (NxN)
 // b) Generate spins in a structured grid
