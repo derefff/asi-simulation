774f7da2454a40df33f668ae14f43c675e376100
derefff <deref122@gmail.com>
2025-04-23 20:58:15 +0200

    todo in table_values.py

--- table_values.py
+++ table_values.py
@@ -1,7 +1,33 @@
 import numpy as np
 from itertools import product
 
-L = 4  # rozmiar sieci 4x4
+# NOTES ---------------
+# żeby pamiętać
+# 1. można zrobić zapis sieci 3x3
+# 2. wygenerowany plik z wartościami bedzie użyty do porównania w głównym pliku symulacji (main.c)
+#
+
+#poprawki
+# 1. są dwie podsieci pion i poziom
+# ze względu na zapis spinów w jednej tablicy (poziom-pion-poziom-pion)
+# wymiar LxL naprawdę to (2L)xL żeby wymiar sie zgadzał
+
+#2. sprawdz is_horizontal bo chyba jest tam zły warunek
+
+#3. J1 i J2 chyba pomyliłem oddziaływania powinienem zrobić zamiane
+# J1 -> J2 i J2 -> J1
+
+#4. Periodic źle działa
+
+#5. zrób pomocniczne wizualizacje sieci żeby mniej wiecej widziec co sie dzieje
+# można wylosować losowy spin i wyswietlic jego sąsiadów
+
+#6. źle obliczana jest magnetyzacja, weź popatrz "wyklad1.pdf" tam jest `smag` tak sie oblicza
+
+#7. wyrysowanie temp dla kilku $\beta$ ? (jeszcze sie dopytam)
+
+
+L = 4  # rozmiar sieci 4x4 -> 8x4
 
 # tworzenie wszystkich konfiguracji spinów (+1 / -1)
 all_configs = list(product([-1, 1], repeat=L * L))
@@ -50,10 +76,11 @@ def calculate_energy(config, J1, J2):
                 E -= J2 * spin * left_down
                 E -= J2 * spin * right_down
 
-    return E / 2 # bo oddziaływania spinow np. 1 - 2, 2 - 1 sa liczone dwa razy 
+    return E / 2 # bo oddziaływania spinow np. 1 - 2, 2 - 1 sa liczone dwa razy
 
 def calculate_magnetization(config):
     mx, my = 0, 0
+    # magnetyzacja nie jest normalizowana
     for y in range(L):
         for x in range(L):
             spin = get_spin(config, x, y)
@@ -65,7 +92,7 @@ def calculate_magnetization(config):
 
 data = []
 J1 = 1.0
-J2 = 0.5
+J2 = 1.0
 beta = 1.0
 
 for config in all_configs:
@@ -81,11 +108,11 @@ with open("config_data.txt", "w") as f:
     f.write("config E mx my weight Z\n")  # nagłówek
     for config, E, mx, my, weight in data:
         config_str = " ".join(map(str, config))
-        f.write(f"{config_str} {E:.6f} {mx} {my} {weight:.6e} {Z:.6e}\n")
+        f.write(f"{config_str} {E:.3f} {mx} {my} {weight:.6e} {Z:.6e}\n")
 
 # srednie wartosci
 avg_mx = sum(mx * w for _, _, mx, _, w in data) / Z
 avg_my = sum(my * w for _, _, _, my, w in data) / Z
 
-print("Funkcja podziału Z:", Z)
-print("Średnia magnetyzacja:", avg_mx, avg_my)
+# print("Funkcja podziału Z:", Z)
+# print("Średnia magnetyzacja:", avg_mx, avg_my)
